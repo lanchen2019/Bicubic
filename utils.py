c0df29c6851407e36f7c8f4b10c6f9f0739da64d
lanchen2019 <46516710+lanchen2019@users.noreply.github.com>
2019-04-04 11:56:17 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -11,13 +11,16 @@ from PIL import Image, ImageOps
 import ast
 
 def get_psnr(pred, gt, shave_border=0):
+    pred = pred.astype(float)
+    gt = gt.astype(float)
     height, width = pred.shape[:2]
     pred = pred[shave_border:height - shave_border, shave_border:width - shave_border]
     gt = gt[shave_border:height - shave_border, shave_border:width - shave_border]
     imdff = pred - gt
-    rmse = math.sqrt(np.mean(imdff ** 2))
+    rmse = math.sqrt(np.mean(imdff ** 2.0))
     if rmse == 0:
         return 100
+    #print(20 * math.log10(255.0 / rmse))
     return 20 * math.log10(255.0 / rmse)
 
 def get_files_in_directory(path):
